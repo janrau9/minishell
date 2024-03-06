ce1d63a9cc6fbac17f7f2c930f6f0f4af806f1be
Janrau Beray <jberay@c1r6p18.hive.fi>
2024-03-06 12:07:51 +0200

    tried loop forks and pipes

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: jberay <jberay@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 11:41:38 by jberay            #+#    #+#             */
-/*   Updated: 2024/03/06 10:45:41 by jberay           ###   ########.fr       */
+/*   Updated: 2024/03/06 12:06:46 by jberay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,119 +14,128 @@
 #include "char_iter.h"
 #include "token.h"
 
-void	parse(t_token **token);
-// #define PROCESS_NUM 1
-// #define RD 0
-// #define WR 1
 
-// /*create pipes*/
-// void	create_pipes(int pipes[][2])
-// {
-// 	int	i;
+#define PROCESS_NUM 1
+#define RD 0
+#define WR 1
 
-// 	i = -1;
-// 	while (++i < PROCESS_NUM + 1)
-// 	{
-// 		if (pipe(pipes[i]) == -1)
-// 		{
-// 			printf("Error with creating pipe\n");
-// 			exit(1);
-// 		}
-// 	}
-// }
+/*create pipes*/
+void	create_pipes(int pipes[][2])
+{
+	int	i;
 
-// /*route child pipe and close unused pipes*/
-// void	route_child_pipe(int pipes[][2], int i)
-// {
-// 	int	j;
+	i = -1;
+	while (++i < PROCESS_NUM + 1)
+	{
+		if (pipe(pipes[i]) == -1)
+		{
+			printf("Error with creating pipe\n");
+			exit(1);
+		}
+	}
+}
 
-// 	if (i == 0)
-// 	{
-// 		j = -1;
-// 		while (++j < PROCESS_NUM + 1)
-// 		{
-// 			if (i != j)
-// 				close(pipes[j][RD]);
-// 			close(pipes[j][WR]);
-// 		}
-// 	}
-// 	else
-// 	{
-// 		j = -1;
-// 		while (++j < PROCESS_NUM + 1)
-// 		{
-// 			if (i != j)
-// 				close(pipes[j][RD]);
-// 			if (i - 1 != j)
-// 				close(pipes[j][WR]);
-// 		}
-// 	}
-// }
+/*route child pipe and close unused pipes*/
+void	route_child_pipe(int pipes[][2], int i)
+{
+	int	j;
 
-// void	create_procs(int *pids, int pipes[][2], char **envp, char **cmd)
-// {
-// 	int	i;
+	if (i == 0)
+	{
+		j = -1;
+		while (++j < PROCESS_NUM + 1)
+		{
+			if (i != j)
+				close(pipes[j][RD]);
+			close(pipes[j][WR]);
+		}
+	}
+	else
+	{
+		j = -1;
+		while (++j < PROCESS_NUM + 1)
+		{
+			if (i != j)
+				close(pipes[j][RD]);
+			if (i - 1 != j)
+				close(pipes[j][WR]);
+		}
+	}
+}
 
-// 	i = -1;
-// 	while (++i < PROCESS_NUM)
-// 	{
-// 		pids[i] = fork();
-// 		if (pids[i] == -1)
-// 		{
-// 			printf("Error with creating process\n");
-// 			exit(2);
-// 		}
-// 		if (pids[i] == 0)
-// 		{
-// 			route_child_pipe(pipes, i);
-// 			dup2(pipes[i][RD], STDIN_FILENO);
-// 			dup2(pipes[i + 1][WR], STDOUT_FILENO);
-// 			close(pipes[i][RD]);
-// 			close(pipes[i + 1][WR]);
-// 			execve("/bin/echo", cmd, envp);
-// 			exit (0);
-// 		}
-// 	}
-// }
+void	create_procs(int *pids, int pipes[][2], char **envp, char **cmd)
+{
+	int	i;
+
+	i = -1;
+	while (++i < PROCESS_NUM)
+	{
+		pids[i] = fork();
+		if (pids[i] == -1)
+		{
+			printf("Error with creating process\n");
+			exit(2);
+		}
+		if (pids[i] == 0)
+		{
+			route_child_pipe(pipes, i);
+			dup2(pipes[i][RD], STDIN_FILENO);
+			dup2(pipes[i + 1][WR], STDOUT_FILENO);
+			close(pipes[i][RD]);
+			close(pipes[i + 1][WR]);
+			execve("/bin/echo", cmd, envp);
+			exit (0);
+		}
+	}
+}
+// loooping through the pipes, forks, and closing the read and write ends
 
 int	main(int argc, char **argv, char **envp)
 {
-	char	*read_line;
-	t_token	*token;
-	
-	// int		pids[PROCESS_NUM];
-	// int		pipes[PROCESS_NUM][2];
-	// int		i;
-	// char	*cmd[] = {"/bin/echo", "hello", NULL};
 
-	(void)argc;
-	(void)argv;
-	(void)envp;
-	while (1)
+	int		pids[PROCESS_NUM];
+	int		pipes[PROCESS_NUM][2];
+	int		i;
+	char	*cmd[] = {"/bin/echo", "hello", NULL};
+	create_pipes(pipes);
+	create_procs(pids, pipes, envp, cmd);
+	i = -1;
+	while (++i < PROCESS_NUM + 1)
 	{
-		read_line = readline ("janrau:~$");
-		if (ft_strncmp(read_line, "exit", 4) == 0)
-		{
-			free(read_line);
-			printf("Exiting minishell\n");
-			exit (0);
-		}
-		add_history(read_line);
-		tokenizer(read_line, &token);
-		parse(&token);
-		free(read_line);
-		free(token);
-		// create_pipes(pipes);
-		// create_procs(pids, pipes, envp, cmd);
-		// i = -1;
-		// while (++i < PROCESS_NUM + 1)
-		// {
-		// 	close(pipes[i][RD]);
-		// 	close(pipes[i][WR]);
-		// }
-		// i = -1;
-		// while (++i < PROCESS_NUM)
-		// 	wait(NULL);
+		close(pipes[i][RD]);
+		close(pipes[i][WR]);
 	}
+	i = -1;
+	while (++i < PROCESS_NUM)
+		wait(NULL);
 	return (0);
 }
+
+// parsing part
+// void	parse(t_token **token);
+
+// int	main(int argc, char **argv, char **envp)
+// {
+// 	char	*read_line;
+// 	t_token	*token;
+
+// 	(void)argc;
+// 	(void)argv;
+// 	(void)envp;
+// 	while (1)
+// 	{
+// 		read_line = readline ("janrau:~$");
+// 		if (ft_strncmp(read_line, "exit", 4) == 0)
+// 		{
+// 			free(read_line);
+// 			printf("Exiting minishell\n");
+// 			exit (0);
+// 		}
+// 		add_history(read_line);
+// 		tokenizer(read_line, &token);
+// 		parse(&token);
+// 		free(read_line);
+// 		free(token);
+// 	}
+// 	return (0);
+// }
