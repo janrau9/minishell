4a1e293a57a964ef80264ad69e3948d56f412bce
Jingyi Tu <jingyi.tu@outlook.com>
2024-03-07 20:52:22 +0200

    edit

--- executor/executor.c
+++ executor/executor.c
@@ -6,18 +6,22 @@
 /*   By: jtu <jtu@student.hive.fi>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 14:46:40 by jtu               #+#    #+#             */
-/*   Updated: 2024/03/07 17:56:22 by jtu              ###   ########.fr       */
+/*   Updated: 2024/03/07 19:28:06 by jtu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "executor.h"
 
-void	executor(t_list *parsed_cmd)
+/*get the command from parser and create pipes and child process*/
+void	executor(t_list *parsed_cmd, t_util *util)
 {
 	t_cmd	*cmd;
 	int		fd[2];
 	int		*pid;
 	int		i;
+	int		j;
+	int		fd_in;
+	int		status;
 
 	i = 0;
 	cmd = (t_cmd *)parsed_cmd->content;
@@ -26,6 +30,24 @@ void	executor(t_list *parsed_cmd)
 		if (parsed_cmd->next)
 			pipe(fd);
 		pid[i] = fork();
+		if (pid[i] < 0)
+			error(); //
+		if (util->here_doc)
+		{
+			close(fd[0]);
+			fd_in = open();
+		}
+		else
+			fd_in = fd[0];
 		parsed_cmd = parsed_cmd->next;
+		i++;
 	}
+	j = 0;
+	while (j++ < i)
+		waitpid(pid[i], NULL, 0);
+	waitpid(pid[i], &status, 0);
+	if (WIFEXITED(status))
+		util->exit_code = WEXITSTATUS(status);
+	else if (WIFSIGNALED(status))
+		util->exit_code = WTERMSIG(status);
 }
--- executor/executor.h
+++ executor/executor.h
@@ -6,7 +6,7 @@
 /*   By: jtu <jtu@student.hive.fi>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 17:32:17 by jtu               #+#    #+#             */
-/*   Updated: 2024/03/07 17:44:32 by jtu              ###   ########.fr       */
+/*   Updated: 2024/03/07 19:27:47 by jtu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,19 @@
 # define EXECUTOR_H
 
 # include "../libft/libft.h"
+# include "../minishell.h"
 
 typedef struct s_cmd
 {
 	char	**cmd;
 	char	**redirection;
 }	t_cmd;
+
+typedef struct s_util
+{
+	bool	here_doc;
+	int		pipes;
+	int		exit_code;
+}	t_util;
 
 #endif
