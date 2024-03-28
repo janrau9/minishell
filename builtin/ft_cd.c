59a8eecf2b15695d1ae2eb56b8bb1058fad205d6
Janrau Beray <jberay@c1r6p20.hive.fi>
2024-03-28 10:16:36 +0200

    cd

--- builtin/ft_cd.c
+++ builtin/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: jberay <jberay@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 15:56:50 by jtu               #+#    #+#             */
-/*   Updated: 2024/03/27 15:29:14 by jberay           ###   ########.fr       */
+/*   Updated: 2024/03/28 10:16:04 by jberay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,12 +30,8 @@ int	ft_cd(t_exec *exec, char **cmd)
 	char		buffer[1024];
 	char		*old[3];
 
+
 	getcwd(buffer, 1024);
-	old[0] = "export";
-	old[1] = ft_strjoin("OLDPWD=", buffer);
-	if (!old[1])
-		ft_error(exec, "malloc error", MALLOC_ERROR);
-	old[2] = NULL;
 	if (ft_arrlen(cmd) > 2)
 		return (0);
 	if (!ft_strncmp(cmd[1], "~", 2))
@@ -45,16 +41,21 @@ int	ft_cd(t_exec *exec, char **cmd)
 		if (S_ISDIR(buf.st_mode))
 		{
 			if (chdir(cmd[1]) == -1)
-				error_exit(exec, EXECVE_FAIL, cmd[1]);
+				return (error_exit(exec, EXECVE_FAIL, cmd[1]));
 		}
 		else
-			error_exit(exec, NO_PATH, cmd[1]);
+			return (error_exit(exec, NO_PATH, cmd[1]));
 	}
 	else
-		error_exit(exec, STAT_FAIL, cmd[1]);
+		return (error_exit(exec, STAT_FAIL, cmd[1]));
+	old[0] = "export";
+	old[1] = ft_strjoin("OLDPWD=", buffer);
+	if (!old[1])
+		ft_error(exec, "malloc error", MALLOC_ERROR);
+	old[2] = NULL;
 	ft_export(exec, old);
 	free(old[1]);
-	
+
 	getcwd(buffer, 1024);
 	old[1] = ft_strjoin("PWD=", buffer);
 	if (!old[1])
